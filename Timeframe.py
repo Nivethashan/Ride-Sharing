bbee16e82c0b7c57b7725fa0e65072612f46c75d
Nivethashan <nshanm3@uic.edu>
2017-05-02 18:07:33 -0500

    Update Timeframe.py
    Setting Pool size and total end datetime

--- Timeframe.py
+++ Timeframe.py
@@ -5,7 +5,7 @@ starttime=datetime.datetime(2013,12,1,0,0,0) # Setting initial value to pool win
 
 endtime = datetime.datetime(2013,12,1,0,5,0) # Setting initial value to pool window End time
 
-untildatetime = datetime.datetime(2013,12,8,0,0,0) # End date and time
+untildatetime = datetime.datetime(2013,12,8,0,0,0) # End - date and time
 
 windowsize = datetime.timedelta(0,300) # Setting the window size
 
